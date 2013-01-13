71a5f843d398d2a4bc4cd4a4853c22452b31e4cb
Kerwin <961610311@qq.com>
2013-01-13 20:09:51 +0800

    Test v1
    Test desc

---
+++ src/com/xbw/anyclass/User.java
@@ -0,0 +1,36 @@
+package com.xbw.anyclass;
+
+public class User {
+
+	private int id;
+	private String userName;
+	private String userPassword;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public void _method() {
+
+	}
+}
---
+++ src/com/xbw/test/ReflectTest.java
@@ -0,0 +1,25 @@
+package com.xbw.test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import com.xbw.anyclass.User;
+
+public class ReflectTest {
+
+	public static void main(String[] args) {
+
+		Class clazz = User.class;
+		// field¿‡µƒ Ù–‘;
+		Field[] fs = clazz.getDeclaredFields();
+		for (Field field : fs) {
+			System.out.println(field.getName() + "\t" + field.getType());
+		}
+		System.out.println();
+		Method[] ms = clazz.getDeclaredMethods();
+		for (Method method : ms) {
+			System.out
+					.println(method.getName() + "\t" + method.getReturnType());
+		}
+	}
+}
